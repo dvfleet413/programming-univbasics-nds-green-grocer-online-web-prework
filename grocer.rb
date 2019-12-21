28333423c70009dec4aeecea21713ffbf73c7196
David Van Fleet <dvfleet413@gmail.com>
2019-12-21 02:35:36 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -84,7 +84,7 @@ def apply_clearance(cart)
   cart_index = 0
   while cart_index < cart.length do
     if cart[cart_index][:clearance] == true
-      cart[cart_index][:price] *= 0.8
+      cart[cart_index][:price] = round(0.8
     end
     cart_index += 1
   end
