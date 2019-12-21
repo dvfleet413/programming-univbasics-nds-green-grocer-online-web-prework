642225011061eaa1eef486dd4fdfc73bb17f8373
David Van Fleet <dvfleet413@gmail.com>
2019-12-21 02:27:21 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -101,4 +101,14 @@ def checkout(cart, coupons)
   #
   # BEFORE it begins the work of calculating the total (or else you might have
   # some irritated customers
+  
+  pp cart
+  
+  cart = consolidate_cart(cart)
+  cart = apply_coupons(cart, coupons)
+  cart = apply_clearance(cart)
+  
+  pp cart
+  
+  
 end
